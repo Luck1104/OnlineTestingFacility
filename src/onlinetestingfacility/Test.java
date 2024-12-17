4f7c0a8ad89ce75904ac46fe1ebe7f4df6eef98b
Aiden <aiden.rinas@gmail.com>
2024-12-17 01:21:17 -0600

    Created the dumpster fire that is takeTest(). Untested, so good chance it doesn't work.

--- src/onlinetestingfacility/Test.java
+++ src/onlinetestingfacility/Test.java
@@ -2,6 +2,7 @@ package onlinetestingfacility;
 
 import java.sql.*;
 import java.util.Scanner;
+import java.util.ArrayList;
 
 /**
  * Represents a test in the online testing facility
@@ -79,5 +80,94 @@ public class Test {
                 String test_name = rs.getString("test_name");
                 System.out.println(test_id + " - " + test_name);
             }
+    }
+    
+    // Allows a user to take a test and adds their score to the database
+    public static void takeTest(int person_id) throws SQLException {
+        int score_count = 0;
+        int question_count = 1;
+        
+        String viewTests = "SELECT * FROM test";
+        PreparedStatement stmt = DatabaseManager.getConnection().prepareStatement(viewTests);
+        ResultSet rs = stmt.executeQuery();
+            while(rs.next()) {
+                int test_id = rs.getInt("test_id");
+                String test_name = rs.getString("test_name");
+                System.out.println(test_id + " - " + test_name);
+            }
+            
+        System.out.println("Enter the id of the test you wish to take: ");
+        Scanner scan = new Scanner(System.in);
+        int test_id = Integer.parseInt(scan.nextLine());
+        
+        String viewQuestions = "SELECT question_id, score, question_text FROM test_with_questions WHERE test_id = ?";
+        PreparedStatement stmt2 = DatabaseManager.getConnection().prepareStatement(viewQuestions);
+        stmt2.setInt(1, test_id);
+        ResultSet rs2 = stmt2.executeQuery();
+        while(rs2.next()) {
+                int question_id = rs2.getInt("question_id");
+                int score = rs2.getInt("score");
+                String question_text = rs2.getString("question_text");
+                
+                System.out.println(question_count + ". " + question_text);
+                
+                int choice_count = 1;
+                ArrayList<String> choice_texts = new ArrayList<String>();
+                
+                String viewChoices = ("SELECT choice_text FROM questions_with_choices WHERE question_id = ?");
+                PreparedStatement stmt3 = DatabaseManager.getConnection().prepareStatement(viewChoices);
+                stmt3.setInt(1, question_id);
+                ResultSet rs3 = stmt3.executeQuery();
+                while(rs3.next()) {
+                    String choice_text = rs3.getString("choice_text");
+                    System.out.println(" " + choice_count + ". " + choice_text);
+                    choice_count++;
+                    choice_texts.add(choice_text);
+                }
+                
+                System.out.println("Enter the number of your choice: ");
+                Scanner scan2 = new Scanner(System.in);
+                int chosen_choice = Integer.parseInt(scan2.nextLine());
+                
+                String chosen_choice_text = choice_texts.get(chosen_choice-1);
+                String viewChoiceCorrectness = ("SELECT is_correct FROM questions_with_choices "
+                        + "WHERE question_id = ? AND choice_text = ?");
+                PreparedStatement stmt4 = DatabaseManager.getConnection().prepareStatement(viewChoiceCorrectness);
+                stmt4.setInt(1, question_id);
+                stmt4.setString(2, chosen_choice_text);
+                ResultSet rs4 = stmt4.executeQuery();
+                boolean is_correct = rs4.getBoolean("is_correct");
+                if(is_correct) {
+                    score_count += score;
+                }
+                question_count++;
+            }
+        
+        int total_score = totalScore(test_id);
+        System.out.println("You got " + score_count + " points out of " + total_score + ".");
+        
+        String insertScore = "INSERT INTO person_tests (person_id, test_id, test_score) "
+                + "VALUES (?, ?, ?)";
+        PreparedStatement stmt5 = DatabaseManager.getConnection().prepareStatement(insertScore);
+        stmt5.setInt(1, person_id);
+        stmt5.setInt(2, test_id);
+        stmt5.setInt(3, total_score);
+        stmt5.executeUpdate();
+    }
+    
+    // Returns the total score of a specific test
+    private static int totalScore(int test_id) throws SQLException {
+        int score = 0;
+        
+        String viewScores = "SELECT score FROM test_with_questions WHERE test_id = ?";
+        PreparedStatement stmt = DatabaseManager.getConnection().prepareStatement(viewScores);
+        stmt.setInt(1, test_id);
+        ResultSet rs = stmt.executeQuery();
+        while(rs.next()) {
+            int question_score = rs.getInt("score");
+            score += question_score;
+        }
+        
+        return score;
     }
 }
